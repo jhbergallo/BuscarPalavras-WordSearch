0fcbacc08fefda34e4027811b30f9fc3b9c903d5
João Bergallo <jhbergallo@gmail.com>
2023-07-04 14:08:55 -0300

    adicionando ingles

--- src/App.java
+++ src/App.java
@@ -1,19 +1,31 @@
 import java.util.List;
 import java.util.Scanner;
 
-
 public class App {
     public static void main(String[] args) {
         
         WordTree arvore = new WordTree();
         arvore.lerArquivo("dicionario.csv");
 
+        WordTree arvoreIng = new WordTree();
+        arvoreIng.lerArquivo("dictionary.csv");
 
+        int index = 0;
         Scanner in = new Scanner(System.in);
         
         String caracteres = "";
-
-        while (!caracteres.equalsIgnoreCase("N")) {
+        while(index != 1 && index != 2){
+            System.out.println("\nEscolha sua língua: / Choose your language:");
+            System.out.println("\n1. Português     2. English");
+            index = in.nextInt();
+            if(index != 1 || index != 2 || !in.hasNextInt()){
+                System.out.println("\nEscolha entre os dois! / Choose between the two!\n");
+                in.nextLine();
+            }
+        }
+        
+        if(index == 1){
+            while (!caracteres.equalsIgnoreCase("N")) {
             System.out.println("\n----- Busca de Palavras -----\n");
             System.out.print("Digite os caracteres para pesquisa: ");
             caracteres = in.nextLine();
@@ -53,10 +65,57 @@ public class App {
                     System.out.print("Opção: ");
                     caracteres = in.nextLine();
                     System.out.println("\n\n");
+                    }
+                }
+                System.out.println("\n------Fechando a busca... Volte novamente!------\n");
+            } 
+        } else if(index == 2){
+            while (!caracteres.equalsIgnoreCase("N")) {
+            System.out.println("\n----- Word Search -----\n");
+            System.out.print("Type the characters for the search: ");
+            caracteres = in.nextLine();
+
+            List<Palavra> palavras = arvoreIng.searchAll(caracteres);
+
+            if (caracteres.matches(".*\\d.*")) {
+                System.out.println("\nYou typed a number! Insert only letters.\n");
+            } else {
+                if (palavras.isEmpty()) {
+                    System.out.println("\nThere were no words found with the characters provided!\n");
+                    System.out.print("Do you want to search for another word? (Y/N): ");
+                    caracteres = in.nextLine();
+                } else {
+                    System.out.println("\nWords found:");
+                    for (int i = 0; i < palavras.size(); i++) {
+                        System.out.println((i + 1) + ". " + palavras.get(i).getPalavra());
+                    }
+                    System.out.println("\nChoose the number for the word to see its meaning(Type 0 to search for another word or N to close)\n");
+                    System.out.print("Option: ");
+                    String n = in.nextLine();
+                    if (n.equalsIgnoreCase("N")) {
+                        break;
+                    }
+                    int escolha = Integer.parseInt(n);
+                    if (escolha == 0) {
+                        continue;
+                    } else if (escolha > 0 && escolha <= palavras.size()) {
+                        Palavra palavraEscolhida = palavras.get(escolha - 1);
+                        System.out.println("\nMeaning of " + palavraEscolhida.getPalavra() + ":");
+                        System.out.println(palavraEscolhida.getSignificado() + "\n");
+                    } else {
+                        System.out.println("\nInvalid choice!");
+                    }
+
+                    System.out.print("Do you want to search for another word? (Y/N):\n");
+                    System.out.print("Option: ");
+                    caracteres = in.nextLine();
+                    System.out.println("\n\n");
+                    }
                 }
             }
+            System.out.println("\n------Closing the search... Come back again!------\n");
         }
-        System.out.println("\n------Fechando a busca... Volte novamente!------\n");
+    
 
         in.close();
 
